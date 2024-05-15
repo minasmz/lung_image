9bc73028c4c6965ba49228c4436dd7e4f779f258
Mina Samizadeh <mina.smz2016@gmail.com>
2024-05-14 23:40:50 -0400

    Update classification.py

--- classification.py
+++ classification.py
@@ -54,7 +54,7 @@ batch_size = 16
 data_transforms = {
     'train': transforms.Compose([
         transforms.Resize((img_size, img_size)),
-        transforms.RandomHorizontalFlip(),
+        #transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]),
